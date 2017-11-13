9b9116d662e4cefccff611234896479982477bbe
Mukund Kothari <mukundkothari172@gmail.com>
2017-11-13 15:53:54 +0530

    Sorted all nodes code

--- Assignment 2/graph.py
+++ Assignment 2/graph.py
@@ -1,16 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
-file = open('GraphRank.txt')
-label = {line[:1]:line[1:].split() for line in file}
-print label
-file.close()
-
 g = nx.read_edgelist('GraphData.txt',create_using=nx.DiGraph(),nodetype=int)
 
 print nx.info(g)
 
-nx.draw(g,nx.circular_layout(g),with_labels=True, node_color='y',node_size=900,alpha=0.8)
+nx.draw(g,nx.circular_layout(g),with_labels=True, node_color='g',node_size=900,alpha=0.8)
 
 plt.show()
